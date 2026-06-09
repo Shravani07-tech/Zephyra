743994b31e9ce65cb793fd68d405004eb920dd66
Shravani <mayekarshravani31@gmail.com>
2026-06-09 12:49:03 +0530

    Week 1:Python basics Quiz app

--- main.py
+++ main.py
@@ -1,12 +1,16 @@
-name = "Zephyra"
-version = 1.0
-is_alive = True 
-print(f"System:{name}")
-print(f"Version:{version}")
-print(f"Status:{is_alive}")
-for i in range(5):
-    print(f"Zephyra booting...step{i+1}")
-    def greet(system_name):
-        return f"Hello,I am {system_name}.Ready."
-    message =greet(name)
-    print(message)
+questions =[
+    {"question":"What language is Zephyra build in?","answer":"python"},
+    {"question":"What framwork handles the API?","answer":"fastapi"},
+    {"question":"What tool runs LLMs locally?","answer":"ollama"},
+    {"question":"What is the name of the project?","answer":"Zephyra"},
+    {"question":"What stores vector memory?","answer":"chromadb"},
+]
+score = 0
+for q in questions:
+    user_input =input(q["question"] + ">").lower()
+    if user_input ==q["answer"]:
+        print("correct!")
+        score += 1
+    else:
+        print(f"Wrong.Answer was:{q['answer']}")
+        print(f"\nYour score:{score}/5")
